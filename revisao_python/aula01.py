a2950eb684127466ef09d110da2985a311215d40
Moisés Souto <moises.souto@ifrn.edu.br>
2019-02-14 18:05:37 -0300

    Primeiro código de exemplo na revisão de linguagem python

---
+++ revisao_python/aula01.py
@@ -0,0 +1,5 @@
+import sys #importar um módulo (biblioteca)
+print(sys.platform) #Imprime a versão da plataforma
+print(2**100) #Imprime o resultado de 2 elevado a 100
+x='Spam!' #Cria uma variável e armazena
+print(x*8) #??
